e9471004196f12d44a2bb958e8d11f0d3ec18527
Caique Pires <c.pires@ba.estudante.senai.br>
2025-02-13 22:26:42 -0300

    Estrutura do hotel criada: Essa classe serve como um "container" para armazenar informações dos quartos e das reservas, permitindo o gerenciamento desses dados de maneira organizada.

---
+++ src/main/java/Hotel.java
@@ -0,0 +1,7 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Hotel {
+    List<Room>rooms = new ArrayList<>(); // Lista todos os quartos disponíveis e indisponíveis
+    List<Reserva>reservas = new ArrayList<>(); // Lista as reservas feitas
+}
