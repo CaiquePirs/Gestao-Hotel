ac95873985e2c219fdc76a382fd599fc47a153a8
Caique Pires <c.pires@ba.estudante.senai.br>
2025-02-13 22:29:09 -0300

    Classe do quarto criada. Esta classe armazena informações dos quartos, cria um novo quarto e exibe as informações do quarto criado.

---
+++ src/main/java/Room.java
@@ -0,0 +1,26 @@
+public class Room {
+    private  int id;
+    private String type;
+    private int capacity;
+    private double price;
+    private boolean status = false;
+
+    // Método constutor para criar um quarto no sistema do hotel
+    Room(int id, String type, int capacity, double price){
+        this.id = id;
+        this.type = type;
+        this.capacity = capacity;
+        this.price = price;
+    }
+
+    // Método para retornar o status do quarto
+    public String getStatus() {
+        return status ? "Reservado" : "Disponível";
+    }
+
+    // Método para retornar informações do quarto criado
+    public String roomInfo(){
+        return "Id: " + id + " - Tipo: " + type + " - Capacidade: " + capacity + " - Preço: R$" + price + " - Status: " + getStatus();
+    }
+
+}
